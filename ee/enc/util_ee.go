e0d5105c8fe1f0637611374d5d48cab40a13704d
aman-bansal <bansalaman2905@gmail.com>
2021-01-06 16:00:35 +0530

    upgrading badger to v3

--- ee/enc/util_ee.go
+++ ee/enc/util_ee.go
@@ -18,7 +18,7 @@ import (
 	"io"
 	"io/ioutil"
 
-	"github.com/dgraph-io/badger/v2/y"
+	"github.com/dgraph-io/badger/v3/y"
 	"github.com/dgraph-io/dgraph/x"
 	"github.com/golang/glog"
 	"github.com/pkg/errors"
